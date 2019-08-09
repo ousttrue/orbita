67ca65b7d077ed3fc049c672c3861c783902181d
ousttrue <ousttrue@gmail.com>
2019-08-09 13:28:18 +0900

    value_tests

--- tests/value_tests.cpp
+++ tests/value_tests.cpp
@@ -1,14 +1,60 @@
 #include <catch.hpp>
+#include <perilune.h>
 
-unsigned int Factorial(unsigned int number)
-{
-    return number <= 1 ? number : Factorial(number - 1) * number;
-}
+static int g_new = 0;
+static int g_copy = 0;
 
-TEST_CASE("Factorials are computed", "[factorial]")
+struct Value
 {
-    REQUIRE(Factorial(1) == 1);
-    REQUIRE(Factorial(2) == 2);
-    REQUIRE(Factorial(3) == 6);
-    REQUIRE(Factorial(10) == 3628800);
+    int m_id;
+    Value()
+        : m_id(g_new++)
+    {
+    }
+
+    Value(const Value &rhs)
+    {
+        *this = rhs;
+    }
+
+    Value &operator=(const Value &rhs)
+    {
+        ++g_copy;
+        m_id = rhs.m_id;
+        return *this;
+    }
+};
+
+TEST_CASE("value type test", "[value]")
+{
+
+    auto L = luaL_newstate();
+    luaL_openlibs(L);
+
+    {
+        static perilune::UserType<Value> valueType;
+        valueType
+            .StaticMethod("new", []() { return Value(); })
+            .MetaIndexDispatcher([](auto d) {
+                d->Getter("id", &Value::m_id);
+            })
+            .LuaNewType(L);
+        lua_setglobal(L, "Value");
+    }
+
+    luaL_dostring(L, R""(
+
+print('hello')
+local value = Value.new()
+print(value.id)
+
+)"");
+
+    lua_close(L);
+
+    // new in static method
+    // placement new for userdata
+    // copy to userdata from static method return
+    REQUIRE(2 == g_new);
+    REQUIRE(1 == g_copy);
 }
